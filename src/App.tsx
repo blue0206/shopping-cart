d4a25e1809fd76c3e918a4dce04b1b138f2b8c90
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-02-24 17:00:54 +0530

    Add Header component to App and wrap content in a div

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import { ReactElement } from 'react';
-import { Button } from './components';
+import { Button, Header } from './components';
 import styles from "./styles/app.module.css";
 
 function App(): ReactElement {
 
   return (
-    <>
+    <div className={styles.home}>
+      <Header />
       <div className={styles.heroText}>
         <h1 className={styles.h1}>Your shopping spree starts here.</h1>
         <Button className={styles.button}>Let's Go!</Button>
       </div>
-    </>
+    </div>
   );
 }
 
