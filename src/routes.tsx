ca3c82fc3763cddf2c5d237b773df710f46ef481
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-02-24 17:05:56 +0530

    Add routes for Shop and Cart components in routing setup

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,19 @@
 import { createBrowserRouter } from 'react-router-dom';
 import App from './App';
+import { Shop, Cart } from './components';
 
 const router = createBrowserRouter([
     {
         path: '/',
         element: <App />
+    },
+    {
+        path: '/shop',
+        element: <Shop />
+    },
+    {
+        path: '/cart',
+        element: <Cart />
     }
 ]);
 
