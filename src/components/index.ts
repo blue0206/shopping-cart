f26d9c5e8099f1436f7225cbb98b88b21f5fcd01
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-02-24 23:23:18 +0530

    Add Input component export

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,11 @@
+import Input from "./Input";
 import Button from "./Button";
 import Header from "./Header";
 import Shop from "./Shop/Shop";
 import Cart from "./Cart/Cart";
 
 export {
+    Input,
     Button,
     Header,
     Shop, 
