beadd1eb02b3f0e28730b01368d20fee57a6b438
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-02-24 15:30:43 +0530

    Add hero section with introductory text and button in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-import { ReactElement } from 'react'
+import { ReactElement } from 'react';
+import { Button } from './components';
+import styles from "./styles/app.module.css";
 
 function App(): ReactElement {
 
   return (
-    <></>
+    <>
+      <div className={styles.heroText}>
+        <h1 className={styles.h1}>Your shopping spree starts here.</h1>
+        <Button className={styles.button}>Let's Go!</Button>
+      </div>
+    </>
   );
 }
 
