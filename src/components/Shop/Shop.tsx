44c5d59e89fa817e9ff7aa2b7424d6fad42dcb15
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-02-25 00:26:20 +0530

    Integrate useProductData hook to fetch and display products in Shop component

--- src/components/Shop/Shop.tsx
+++ src/components/Shop/Shop.tsx
@@ -1,10 +1,29 @@
 import { ReactElement } from 'react';
+import useProductData from '../../hooks/UseProductData';
 import { Header } from '../';
+import Product from './Product';
+import { ProductType } from '../../interfaces';
 
 function Shop(): ReactElement {
+    const { products } = useProductData();
+
     return (
         <div>
             <Header />
+            <div>
+                {
+                    products && products.map((product: ProductType) => {
+                        return (
+                            <Product 
+                                key={product.id} 
+                                title={product.title} 
+                                price={product.price} 
+                                image={product.image} 
+                            />
+                        )
+                    })
+                }
+            </div>
         </div>
     );
 }
