7af456926ac1a356fcc0ef3a8549487625032ebb
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-02-26 20:25:10 +0530

    Display cart items in Cart component and handle empty cart state
    - Add Cart component
    - Get cart items from Redux store
    - Render cart items in an ordered list
    - Handle empty cart state

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -1,10 +1,36 @@
 import { ReactElement } from 'react';
 import { Header } from "../";
+import CartItem from './CartItem';
+import { useAppSelector } from '../../app/hooks';
 
 function Cart(): ReactElement {
+    const cartItems = useAppSelector(state => state.cart);
+
     return (
         <div>
             <Header />
+            {
+                cartItems.length > 0 ? (
+                    <ol>
+                        {
+                            cartItems.map(item => (
+                                <li>
+                                    <CartItem 
+                                        key={item.id} 
+                                        id={item.id} 
+                                        title={item.title} 
+                                        image={item.image} 
+                                        price={item.price} 
+                                        quantity={item.quantity} 
+                                    />
+                                </li>
+                            ))
+                        }
+                    </ol>
+                ) : (
+                    <div>The cart is empty.</div>
+                )
+            }
         </div>
     );
 }
