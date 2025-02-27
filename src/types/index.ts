5a54ebe50e2f18986e2fdd1d1ade46deb7f57b30
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-02-27 13:56:11 +0530

    Add JSDoc comments to index types for improved documentation

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,10 @@
+/**
+ * @description Type for a product.
+ * @property {string} id - The unique id of the product.
+ * @property {string} title - The title of the product.
+ * @property {number} price - The price of the product.
+ * @property {string} image - The URL of the product's image.
+ */
 export type ProductType = {
     id: string;
     title: string;
@@ -5,6 +12,11 @@ export type ProductType = {
     image: string;
 }
 
+/**
+ * @description Type for a product in the cart.
+ * @extends ProductType
+ * @property {number} quantity - The quantity of the product in the cart.
+ */
 export type CartItem = ProductType & {
     quantity: number;
 }
