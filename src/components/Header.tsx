f87c7d3f23d572b690f1e0dfc0b30f46bd66245e
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-02-27 13:47:28 +0530

    Add JSDoc comments to Header component for improved documentation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,6 +3,19 @@ import { NavLink } from 'react-router-dom';
 import styles from '../styles/header.module.css';
 import { useAppSelector } from '../app/hooks';
 
+/**
+ * Renders a header component with a sticky position at the top of the page
+ * so that header stays visible while scrolling down.
+ * 
+ * It has links to the home page and the shop page on the left side.
+ * 
+ * On the right side, it displays a link to the cart page and the number of 
+ * items in the cart which are fetched from Redux store through useAppSelector
+ * typed hook.
+ * The number of items is hidden if the cart is empty.
+ * 
+ * @returns {ReactElement} - The rendered header component.
+ */
 function Header(): ReactElement {
     const cartQuantity = useAppSelector(state => state.cart.length);
     const itemCounterVisibility = cartQuantity > 0 ? "visible" : "hidden";
