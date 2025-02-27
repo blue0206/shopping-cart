7ba74393ebb648e481bb8abb78b3dd4b85ea30bd
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-02-27 14:04:11 +0530

    Add JSDoc comments to hooks for improved documentation and type safety

--- src/app/hooks.ts
+++ src/app/hooks.ts
@@ -1,5 +1,13 @@
 import { useSelector, useDispatch } from "react-redux";
 import { RootState, AppDispatch } from "./store";
 
+/**
+ * @description
+ * This is a typed custom hook that allows us to use the redux store 
+ * in our components with type safety and no need to pass the types
+ * explicitly to the useSelector and useDispatch hooks.
+ * @returns {useAppSelector} - A function that returns the state of the store.
+ * @returns {useAppDispatch} - A function that dispatches an action to the store.
+ */
 export const useAppSelector = useSelector.withTypes<RootState>();
 export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
