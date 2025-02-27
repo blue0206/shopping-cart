86f56a0ac7eca0d74f08242d978f4ed9b2e19989
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-02-27 13:49:54 +0530

    Add JSDoc comments to Input component for improved documentation

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -2,6 +2,13 @@ import { ReactElement, ComponentProps } from "react";
 
 type InputProps = ComponentProps<'input'>;
 
+/**
+ * A component that renders an input element with all the standard input 
+ * element properties.
+ * 
+ * @param {InputProps} props The properties passable to an input element.
+ * @returns {ReactElement} An input element with the properties applied.
+ */
 function Input({
     ...props
 }: InputProps): ReactElement {
