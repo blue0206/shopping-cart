b946daf06c96d1d359bf7fb2d131b58fa2b39f4f
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-02-27 14:07:58 +0530

    Add JSDoc comments to store configuration and TypeScript definitions for improved documentation

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,12 +1,22 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartSlice from "../features/cart/cartSlice";
 
+/**
+ * Configure redux store.
+ * @returns Store.
+ */
 export const store = configureStore({
     reducer: {
         cart: cartSlice
     }
 });
 
+/**
+ * Typescript definitions.
+ * @type AppStore - Redux store.
+ * @type RootState - Root state.
+ * @type AppDispatch - Dispatch function.
+ */
 export type AppStore = typeof store;
 export type RootState = ReturnType<AppStore['getState']>;
 export type AppDispatch = AppStore["dispatch"];
